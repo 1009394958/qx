2078484c4dbc814f11f727055eb5891b9d5a4d02
1009394958 <76431457+1009394958@users.noreply.github.com>
2026-01-07 08:56:33 +0800

    Modify is_vip and add vip_expire_date handling
    Added check for vip_expire_date and updated its value.

--- modify_is_vip.js
+++ modify_is_vip.js
@@ -1,10 +1,13 @@
 // 解析响应体
 let body = JSON.parse($response.body);
 
-// 检查返回的数据中是否包含 is_vip 字段
-if (body.data && body.data.is_vip !== undefined) {
+// 检查返回的数据中是否包含 is_vip 和 vip_expire_date 字段
+if (body.data) {
     // 将 is_vip 修改为 1，表示用户是 VIP
     body.data.is_vip = 1;
+    
+    // 将 vip_expire_date 修改为 999999，表示 VIP 永久有效
+    body.data.vip_expire_date = 999999;
 }
 
 // 返回修改后的响应体
